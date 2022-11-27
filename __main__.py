5bc7308e08469fc4c6b7b677dc4473c7e72a9779
kostisA <46960556+kostisA@users.noreply.github.com>
2022-11-27 09:45:45 -0600

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,11 @@
+# Team: ***
+# Authors: Jason ***, Ye ***, Konstantinos Alexopoulos
+# Class: COMP 424 ELEC 424/553
+# Final Project: Autonomous RC Car 
+# Fall 2022
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
